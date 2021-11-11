015081e9fe17e36925ac58a09a5bba2ff5ca8109
hexeva <patrick79.g@gmail.com>
2021-11-11 15:02:51 +0100

    declarations

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
+
+// DICHIARAZIONI
+
+// valore password fisso
+const passwordNumber = 21;
+
+
+// Chiedo nome utente
+const userName = prompt ('Come ti chiami?');
+
+// chiedo il cognome all'utente
+const lastName = prompt ('Qual\'è il tuo cognome?');
+
+// chiedo il colore preferito all'utente
+
+const favoriteColor = prompt ('Qual\'è il tuo colore preferito?');
+
+
+
+// CICLO PROGRAMMA
+
+// OUTPUT
